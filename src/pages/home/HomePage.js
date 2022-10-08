7f182aafea17a46a36c4e94a01e5231b3813c237
Emin Azeroglu <azeroglu.developer@gmail.com>
2022-10-09 00:13:47 +0400

    fix: refactoring

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import {Page} from "components/ui";
-import {FormHtmlEditor} from "components/ui/form";
 
 function HomePage(props) {
     return (
         <Page>
-            <FormHtmlEditor/>
+            Home Page
         </Page>
     );
 }
